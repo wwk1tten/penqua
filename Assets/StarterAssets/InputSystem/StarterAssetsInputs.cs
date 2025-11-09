9513621eb169a5c3930eafe00642579a2e729af4
w0n11 <ridiceline@gmail.com>
2025-11-09 16:09:04 +0900

    Feat: 조준기능 추가

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -12,6 +12,7 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool aim;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -43,6 +44,13 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+
+		public void OnAim(InputValue value)
+		{
+			AimInput(value.isPressed);
+		}
+
+		
 #endif
 
 
@@ -64,6 +72,9 @@ namespace StarterAssets
 		public void SprintInput(bool newSprintState)
 		{
 			sprint = newSprintState;
+		}
+		public void AimInput(bool newAimState){
+			aim = newAimState;
 		}
 
 		private void OnApplicationFocus(bool hasFocus)
